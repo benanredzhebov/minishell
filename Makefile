075e1de14a3b2fd6d6e3d42276aca3026c4195c8
Oleh Ruban <oruban@c2r10s9.42wolfsburg.de>
2024-05-29 17:56:25 +0200

    Makefile corrected

--- Makefile
+++ Makefile
@@ -9,16 +9,18 @@ LIBFT	= ./libft/libft.a
 
 $(NAME): $(OBJS) $(LIBFT)
 	$(CC) $(FLAGS) $(OBJS) $(LIBFT) -o $(NAME)
-all:
-	$(NAME)
+
+all: $(NAME)
+
 clean:
 	rm -f $(OBJS)
 	make -C ./libft $@
+
 fclean:
 	rm -f $(OBJS) $(NAME)
 	make -C ./libft $@
-re:
-	fclean all
+
+re:	fclean all
 
 $(LIBFT):
 	make -C ./libft
--- minishell.h
+++ minishell.h
@@ -6,11 +6,13 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 17:32:54 by oruban            #+#    #+#             */
-/*   Updated: 2024/05/15 17:32:57 by oruban           ###   ########.fr       */
+/*   Updated: 2024/05/29 17:49:04 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h> // EXIT_FAILURE
+#include "libft/libft.h"
+#include "libft/ft_printf/ft_printf.h"
 typedef struct s_env
 {
 	char			*key_pair;
